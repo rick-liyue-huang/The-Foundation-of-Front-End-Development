e7cfc6b4f3896f49b45af99d2b76796ab5380af6
Rick Huang <rick.liyue.huang@gmail.com>
2016-07-06 23:06:14 +1000

    Update completedAnimationMethod.js

--- JSAnimation/completedAnimationMethod.js
+++ JSAnimation/completedAnimationMethod.js
@@ -39,3 +39,12 @@
             }
         }, interval)
     }
+    
+    
+    function getStyle(obj, attr) {
+    if (obj.currentStyle) {
+        return obj.currentStyle[attr];
+    } else {
+        return window.getComputedStyle(obj, null)[attr];
+    }
+}

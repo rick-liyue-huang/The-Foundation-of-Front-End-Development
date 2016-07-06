c4114f87d81c83300da8dcf75af80c03ddecd87d
Rick Huang <rick.liyue.huang@gmail.com>
2016-07-06 19:33:37 +1000

    Create clientMethod.js

---
+++ JSAnimation/clientMethod.js
@@ -0,0 +1,20 @@
+//    capsulate the client function to return the clientWidth and clientHeight
+    function client() {
+        
+        if (window.innerWidth != null) {
+            return {
+                'width': window.innerWidth,
+                'height': window.innerHeight
+            };
+        } else if (document.compatMode == 'CSS1Compat') {
+            return {
+                'width': document.documentElement.clientWidth,
+                'height': document.documentElement.clientHeight
+            };
+        } else {
+            return {
+                'width': document.body.clientWidth,
+                'height': document.body.clientHeight
+            };
+        }
+    }

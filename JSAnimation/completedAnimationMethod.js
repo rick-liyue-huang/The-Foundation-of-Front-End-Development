30c4808fe7914d677e84b95eff08010f06909d0f
Rick Huang <rick.liyue.huang@gmail.com>
2016-07-06 21:14:42 +1000

    Create completedAnimationMethod.js

---
+++ JSAnimation/completedAnimationMethod.js
@@ -0,0 +1,41 @@
+
+ // change any object, any attribute to any target value
+    //change more one attribute value simultaneously
+
+    // add callback function to let the object move in continuously
+    function animate(obj, json, fn, interval) {
+        clearInterval(obj.timer);
+        obj.timer = setInterval(function () {
+            var flag = true;//define a flag to true for jumping out condition
+            for (var k in json) {
+                if (k == "opacity") {
+                    //getStyle will get string, will transfer to number
+                    var leader = getStyle(obj, k) * 100;// transfer to int
+                    var target = json[k] * 100;
+                    var step = (target - leader) / 10;
+                    step = step > 0 ? Math.ceil(step) : Math.floor(step);
+                    obj.style[k] = (leader + step) / 100;
+                } else if (k == "zIndex") {
+                    obj.style[k] = json[k];
+                } else {
+                // k -> attr,  json[k] -> target
+                    var leader = parseInt(getStyle(obj, k)) || 0;
+                    var target = json[k];
+                    var step = (target - leader) / 10;
+                    step = step > 0 ? Math.ceil(step) : Math.floor(step);
+                    obj.style[k] = leader + step + "px";
+                }
+
+
+                if (leader != target) {
+                    flag = false;// will keep moving 
+                }
+            }
+            if (flag) {// flag == true
+                clearInterval(obj.timer);
+                if (fn) {
+                    fn(); // callback function
+                }
+            }
+        }, interval)
+    }

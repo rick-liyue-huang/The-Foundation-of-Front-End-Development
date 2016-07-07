b6336fdbbacd9c91ca941689b49398ac50b2f4dc
Rick Huang <rick.liyue.huang@gmail.com>
2016-07-07 11:44:09 +1000

    Update Animation.js

--- JSAnimation/Animation.js
+++ JSAnimation/Animation.js
@@ -1,15 +1,15 @@
 // this capsulating function will provide a method to animated move the element.
 
 function animate(obj, target) {
-        clearInterval(obj.timer);//为了防止重复调用
+        clearInterval(obj.timer);
         obj.timer = setInterval(function () {
             var leader = obj.offsetTop;
             var step = (target - leader) / 10;
             step = step > 0 ? Math.ceil(step) : Math.floor(step);
             console.log("target:" + target + "---leader:" + leader + "---step:" + step);
-            if (leader != target) {//说明还没有到
+            if (leader != target) {
                 obj.style.top = leader + step + "px";
-            } else {//说明到了
+            } else {
                 clearInterval(obj.timer);
             }
         }, 15)
